ae88e9697dfca6afb8e347d64bfa0b24f3d387c9
Dario <rubendario.sotomontana@tiernoparla.es>
2022-12-13 21:43:02 +0100

    Lanzamos los tests para ver si funcionan

---
+++ src/test/java/daw/programa/calculadora/CalculadoraTest.java
@@ -0,0 +1,17 @@
+package daw.programa.calculadora;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+    
+    @Test
+    public void dosSumarDosEsCuatro() {
+        int expected = 4;
+        int numero1 = 2, numero2 = 2;
+        Calculadora calc = new Calculadora();
+
+        assertEquals(expected, calc.sumar(numero1, numero2));
+    }
+}
