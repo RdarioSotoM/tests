5c9d4d416ec3c7e598a30eaa95e5dc22b4f22c3d
Dario <rubendario.sotomontana@tiernoparla.es>
2022-12-14 18:16:26 +0100

    problemas con los test

--- src/main/java/daw/programa/calculadora/Calculadora.java
+++ src/main/java/daw/programa/calculadora/Calculadora.java
@@ -4,5 +4,5 @@ public class Calculadora {
     public int sumar(int numero1, int numero2){
         return numero1 + numero2;
     }
-    
+
 }
--- src/test/java/daw/programa/calculadora/CalculadoraTests.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-
-package daw.programa.calculadora;
-
-public class CalculadoraTests {
-    
-}
