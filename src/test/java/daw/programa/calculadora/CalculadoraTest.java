f52e182ffc6a881b2bc31a2bd1b04130d58b607e
Dario <rubendario.sotomontana@tiernoparla.es>
2022-12-19 21:19:35 +0100

    ponía que estaba modificado, aunque no toqué na

--- src/test/java/daw/programa/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programa/calculadora/CalculadoraTest.java
@@ -2,8 +2,6 @@ package daw.programa.calculadora;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTest {
